17614bdb2bf5b89977b37162344e0ac0e6cd2a11
kavrohit <kashyaprkav7@gmail.com>
2022-10-17 18:36:49 +0530

    added new test to user component

--- client/src/components/user/user.test.tsx
+++ client/src/components/user/user.test.tsx
@@ -16,15 +16,17 @@ describe("Users",() =>{
   test('renders users',async()=>{
     render(<Users/>)
     const users = await screen.findAllByRole('listitem');
-    expect(users).toHaveLength(10);
+    expect(users).toHaveLength(3);
   })
   
-  test('error handling',()=>{
+  test('error handling',async ()=>{
     server.use(
       rest.get('https://jsonplaceholder.typeicode.com/users',(req,res,ctx) =>{
         return res(ctx.status(500))
       })
     );
     render(<Users/>);
-  })
+    const error = await screen.findByText('Error fetching users');
+    expect(error).toBeInTheDocument();
+  });
 })
